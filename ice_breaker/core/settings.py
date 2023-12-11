694e9b4733b07e0cc60e7b31562abc965ca980a7
basileazh <basile.elazhari@gmail.com>
2023-12-11 22:45:02 +0000

    settings.py: added settings for logging, and Twitter via APIFY

--- ice_breaker/core/settings.py
+++ ice_breaker/core/settings.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -9,8 +10,15 @@ load_dotenv()
 
 DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
+logging.basicConfig(level=logging.INFO)
+
 
 class Settings(BaseConfig):
+    # Logging
+    log_file_path = "outputs/logs/[DATETIME_PLACEHOLDER]_log.txt"
+    log_file_path_datetime_format = "%Y-%m-%d_%H"
+    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
     # LinkedIn API (Proxycurl)
     environment = Environment(os.getenv("ENVIRONMENT", "development"))
     if os.getenv("ENVIRONMENT") == "production":
@@ -18,6 +26,22 @@ class Settings(BaseConfig):
         linkedin_api_key = os.getenv("PROXYCURL_API_KEY")
     else:
         path_to_linkedin_sample_profile = os.path.join(DIR, "test", "linkedin_sample_profile.json")
+
+    # Twitter API (Apify)
+    twitter_fields_user = [
+        "name",
+        "description",
+        "followers_count",
+        "friends_count",
+        "profile_image_url_https",
+        "verified",
+    ]
+    twitter_fields_pinned_tweet = [
+        "full_text",
+        "retweet_count",
+        "favorite_count",
+        "reply_count",
+    ]
 
 
 def get_settings(item: str):
